ee22d7d1f116e2146d45b20143ee564d28a63d2e
endiko <kogay.nadezhda@gmail.com>
2019-12-02 16:14:40 +0500

    modified day variable and split results on 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-const day = '01';
+const day = '02'; // Change a day number to get the result
+
 const path = `./day-${day}`;
 
 const part1 = require(`${path}/part1`);
@@ -8,4 +9,7 @@ const part2 = require(`${path}/part2`);
 
 const data = fs.readFileSync(`${path}/input.txt`, 'utf8');
 
-console.log(part1(data), part2(data));
+console.log(part1(data));
+
+// Day 2 part 2
+console.log(part2(data, 19690720));
