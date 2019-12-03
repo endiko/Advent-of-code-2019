13177b137d7e874c72d869d63eabc29ae58bc3ca
endiko <kogay.nadezhda@gmail.com>
2019-12-03 22:12:07 +0500

    modify file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const part2 = require(`${path}/part2`);
 const data = fs.readFileSync(`${path}/input.txt`, 'utf8');
 
 console.log(part1(data));
+console.log(part2(data));
 
 // Day 2 part 2
 // console.log(part2(data, 19690720));
