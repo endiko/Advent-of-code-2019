4282c5918b94d70e00092a68c6aa9cd7a9674f20
endiko <kogay.nadezhda@gmail.com>
2019-12-03 22:28:41 +0500

    refactoring part 1 and part 2

--- day-03/part1.js
+++ day-03/part1.js
@@ -1,4 +1,5 @@
 const manhattanDistance = require('../manhattanDistance');
+const filledWiresPath = require('./filledWiresPath');
 
 module.exports = inputData => {
   const wiresPath = inputData
@@ -10,57 +11,16 @@ module.exports = inputData => {
   let x0 = 0;
   let y0 = 0;
 
-  const filledWiresPath = wire => {
-    let filledGrid = new Set();
-    let xw = x0,
-      yw = y0;
-    let i = 0;
-    while (i < wire.length) {
-      let direction = wire[i][0];
-      let stepsAmount = parseInt(wire[i].slice(1));
-      let dx = 0,
-        dy = 0;
-
-      for (let step = 0; step < stepsAmount; step++) {
-        /* wire's path directions: 'R' - right, 'L' - left, 'U' - up, 'D' - down    */
-        switch (direction) {
-          case 'R': {
-            dx = 1;
-            break;
-          }
-          case 'L': {
-            dx = -1;
-            break;
-          }
-          case 'U': {
-            dy = 1;
-            break;
-          }
-          case 'D': {
-            dy = -1;
-          }
-        }
-        xw += dx;
-        yw += dy;
-
-        filledGrid.add(`${xw},${yw}`);
-      }
-
-      i++;
-    }
-
-    return filledGrid;
-  };
-
   let filledGrid1 = filledWiresPath(wiresPath[0]);
   let filledGrid2 = filledWiresPath(wiresPath[1]);
 
   //   find intersection and calc min manhattanDistance to central port
-  filledGrid1.forEach(point => {
+  for (let point of filledGrid1.keys()) {
     if (filledGrid2.has(point)) {
       let [x, y] = point.split(',');
       minDinstanse = Math.min(manhattanDistance(x, y, x0, y0), minDinstanse);
     }
-  });
+  }
+
   return minDinstanse;
 };
--- day-03/part2.js
+++ day-03/part2.js
@@ -1,46 +1,4 @@
-const filledWiresPath = wire => {
-  let filledGrid = new Map();
-  let xw = 0,
-    yw = 0;
-  let i = 0;
-  let distanse = 0;
-  while (i < wire.length) {
-    let direction = wire[i][0];
-    let stepsAmount = parseInt(wire[i].slice(1));
-    let dx = 0,
-      dy = 0;
-
-    for (let step = 0; step < stepsAmount; step++) {
-      /* wire's path directions: 'R' - right, 'L' - left, 'U' - up, 'D' - down    */
-      switch (direction) {
-        case 'R': {
-          dx = 1;
-          break;
-        }
-        case 'L': {
-          dx = -1;
-          break;
-        }
-        case 'U': {
-          dy = 1;
-          break;
-        }
-        case 'D': {
-          dy = -1;
-        }
-      }
-      xw += dx;
-      yw += dy;
-      distanse++;
-
-      filledGrid.set(`${xw},${yw}`, distanse);
-    }
-
-    i++;
-  }
-
-  return filledGrid;
-};
+const filledWiresPath = require('./filledWiresPath');
 
 module.exports = inputData => {
   const wiresPath = inputData
@@ -51,8 +9,6 @@ module.exports = inputData => {
 
   let filledGrid1 = filledWiresPath(wiresPath[0]);
   let filledGrid2 = filledWiresPath(wiresPath[1]);
-
-  let intersections = new Set();
 
   filledGrid1.forEach((distanse, point) => {
     if (filledGrid2.has(point)) {
