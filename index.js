cf23fb3e97594fa0c239adde3a16b2f470346d9d
endiko <kogay.nadezhda@gmail.com>
2019-12-08 16:59:04 +0500

    Uncommenting line console.log(part1)

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const part2 = require(`${path}/part2`);
 const data = fs.readFileSync(`${path}/input.txt`, 'utf8');
 // const data = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0';
 
-// console.log(part1(data));
+console.log(part1(data));
 console.log(part2(data));
 
 // Day 2 part 2
