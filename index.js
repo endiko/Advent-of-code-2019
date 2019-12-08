0473a617e95b43f3aef72446da037e737cf9fab7
endiko <kogay.nadezhda@gmail.com>
2019-12-08 16:42:16 +0500

    Modified for day 8 part 2

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const part2 = require(`${path}/part2`);
 const data = fs.readFileSync(`${path}/input.txt`, 'utf8');
 // const data = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0';
 
-console.log(part1(data));
-// console.log(part2(data));
+// console.log(part1(data));
+console.log(part2(data));
 
 // Day 2 part 2
 // console.log(part2(data, 19690720));
