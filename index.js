c8cba4ec62c51423c84c852514a58a701c816b77
endiko <kogay.nadezhda@gmail.com>
2019-12-08 14:16:47 +0500

    modify a day number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const day = '06'; // Change a day number to get the result
+const day = '08'; // Change a day number to get the result
 
 const path = `./day-${day}`;
 
@@ -8,9 +8,10 @@ const part1 = require(`${path}/part1`);
 const part2 = require(`${path}/part2`);
 
 const data = fs.readFileSync(`${path}/input.txt`, 'utf8');
+// const data = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0';
 
 console.log(part1(data));
-console.log(part2(data));
+// console.log(part2(data));
 
 // Day 2 part 2
 // console.log(part2(data, 19690720));
