f451a427d873a325e07b3dcf70914c1edd185fae
endiko <kogay.nadezhda@gmail.com>
2019-12-09 18:18:24 +0500

    Modified for day 9

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const day = '08'; // Change a day number to get the result
+const day = '09'; // Change a day number to get the result
 
 const path = `./day-${day}`;
 
@@ -10,8 +10,8 @@ const part2 = require(`${path}/part2`);
 const data = fs.readFileSync(`${path}/input.txt`, 'utf8');
 // const data = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0';
 
-console.log(part1(data));
-console.log(part2(data));
+// console.log(part1(data));
+// console.log(part2(data));
 
 // Day 2 part 2
 // console.log(part2(data, 19690720));
@@ -23,3 +23,7 @@ console.log(part2(data));
 // Day 5
 // console.log(part1(data, 1));
 // console.log(part2(data, 5));
+
+// Day 9
+// console.log(part1(data, 1));
+console.log(part2(data, 2));
