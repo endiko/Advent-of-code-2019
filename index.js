bf6a91781873fa325e6736b7de246ce6f12a9003
endiko <kogay.nadezhda@gmail.com>
2019-12-25 13:49:40 +0500

    Last change od a day

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const day = '09'; // Change a day number to get the result
+const day = '12'; // Change a day number to get the result
 
 const path = `./day-${day}`;
 
@@ -8,7 +8,6 @@ const part1 = require(`${path}/part1`);
 const part2 = require(`${path}/part2`);
 
 const data = fs.readFileSync(`${path}/input.txt`, 'utf8');
-// const data = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0';
 
 // console.log(part1(data));
 // console.log(part2(data));
@@ -26,4 +25,21 @@ const data = fs.readFileSync(`${path}/input.txt`, 'utf8');
 
 // Day 9
 // console.log(part1(data, 1));
-console.log(part2(data, 2));
+// console.log(part2(data, 2));
+
+// Day 10
+// const data = `.#..#
+//         .....
+//         #####
+//         ....#
+//         ...##`;
+
+//  Day 12
+
+// console.log(part1(data, 1000));
+
+// const data = `<x=-1, y=0, z=2>
+// <x=2, y=-10, z=-7>
+// <x=4, y=-8, z=8>
+// <x=3, y=5, z=-1>`;
+console.log(part2(data));
