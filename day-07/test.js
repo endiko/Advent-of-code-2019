4ab2b2cea5c075553927a527318ccfada9c33d8f
endiko <kogay.nadezhda@gmail.com>
2019-12-25 13:50:47 +0500

    Add test for day 7

---
+++ day-07/test.js
@@ -0,0 +1,21 @@
+// const assert = require('assert');
+// const part1 = require('./part1');
+// const part2 = require('./part2');
+
+// describe('--- Day 7: Amplification Circuit --- Part 1: ', () => {
+//   it('Max thruster signal (from phase setting sequence 4,3,2,1,0) should return 43210', () => {
+//     const data = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0';
+//     assert.equal(part1(data), 43210);
+//   });
+
+//   it('Max thruster signal (from phase setting sequence 0,1,2,3,4) should return 54321', () => {
+//     const data =
+//       '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0';
+//     assert.equal(part1(data), 54321);
+//   });
+//   it('Max thruster signal (from phase setting sequence 1,0,4,3,2) should return 65210', () => {
+//     const data =
+//       '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0';
+//     assert.equal(part1(data), 65210);
+//   });
+// });
